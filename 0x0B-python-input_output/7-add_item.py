815ba0848a1aefeabddab627db96d020baa94cab
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-11-07 14:12:44 -0800

    correcting errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,27 @@
 """Add all arguments to a Python list and save them to a file."""
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+import json
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+def add_arguments_to_list_and_save(*args):
+    filename = "add_item.json"
+
+    if os.path.exists(filename):
+        data = load_from_json_file(filename)
+        if not isinstance(data, list):
+            data = []
+    else:
+        data=[]
+    data.extend(args)
+
+    save_to_json_file(data, filename)
+
+if __name__ == "__main__":
+    
+    import sys
+    args = sys.argv[1:]
+
+    add_arguments_to_list_and_save(*args)
+
